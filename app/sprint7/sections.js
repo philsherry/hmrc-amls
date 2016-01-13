56f76fd66cadf2d740a4b8e89270fa5bd5be1550
Brian Coult <Etheya@Brians-MacBook-Pro.local>
2016-01-13 15:59:07 +0000

    Various changes to sections and summary

--- app/sprint7/sections.js
+++ app/sprint7/sections.js
@@ -1,10 +1,10 @@
 module.exports = [
   {
     id: 0,
-    name: 'Business activities',
-    section: 'business-activities',
-    link: 'business-activities/index',
-    message: 'your details, details of each address where you carry on business activities',
+    name: 'Business matching',
+    section: 'business-matching',
+    link: 'business-matching/index',
+    message: 'covered by the Money Laundering Regulations.',
     time: '30',
     status: undefined
   },
@@ -19,60 +19,15 @@ module.exports = [
   },
   {
     id: 2,
-    name: 'Accountancy service provider information',
-    section: 'accountancy-service-provider',
-    link: 'accountancy-service-provider/index',
-    message: 'your details, details of each address',
+    name: 'Business activities',
+    section: 'business-activities',
+    link: 'business-activities/index',
+    message: 'your details, details of each address where you carry on business activities',
     time: '30',
     status: undefined
   },
   {
     id: 3,
-    name: 'Estate agent business information',
-    section: 'estate-agents',
-    link: 'estate-agents/index',
-    message: 'by the Money Laundering Regulations, details of staff and agents.',
-    time: '30',
-    status: undefined
-  },
-  {
-    id: 4,
-    name: 'High value dealer information',
-    section: 'high-value-dealer',
-    link: 'high-value-dealer/index',
-    message: 'address where you carry on business activities covered by the Money Laundering Regulations, details of staff and agents.',
-    time: '30',
-    status: undefined
-  },
-  {
-    id: 5,
-    name: 'Money service business information',
-    section: 'money-service-business',
-    link: 'money-service-business/index',
-    message: 'details of staff and agents.',
-    time: '30',
-    status: undefined
-  },
-  {
-    id: 6,
-    name: 'Trust and company service provider information',
-    section: 'trust-company-service-provider',
-    link: 'trust-company-service-provider/index',
-    message: 'activities covered by the Money Laundering Regulations, details of staff and agents.',
-    time: '30',
-    status: undefined
-  },
-  {
-    id: 7,
-    name: 'Trading premises',
-    section: 'trading-premises',
-    link: 'trading-premises/index',
-    message: 'details of each address where you carry on.',
-    time: '30',
-    status: undefined
-  },
-  {
-    id: 8,
     name: 'Bank account details',
     section: 'bank-details',
     link: 'bank-details/index',
@@ -81,7 +36,7 @@ module.exports = [
     status: undefined
   },
   {
-    id: 9,
+    id: 4,
     name: 'Responsible people',
     section: 'responsible-people',
     link: 'responsible-people/index',
@@ -90,7 +45,61 @@ module.exports = [
     status: undefined
   },
   {
+    id: 5,
+    name: 'Trading premises',
+    section: 'trading-premises',
+    link: 'trading-premises/index',
+    message: 'details of each address where you carry on.',
+    time: '30',
+    status: undefined
+  },
+  {
+    id: 6,
+    name: 'Accountancy service provider',
+    section: 'accountancy-service-provider',
+    link: 'accountancy-service-provider/index',
+    message: 'your details, details of each address',
+    time: '30',
+    status: undefined
+  },
+  {
+    id: 7,
+    name: 'Estate agent business',
+    section: 'estate-agents',
+    link: 'estate-agents/index',
+    message: 'by the Money Laundering Regulations, details of staff and agents.',
+    time: '30',
+    status: undefined
+  },
+  {
+    id: 8,
+    name: 'High value dealer',
+    section: 'high-value-dealer',
+    link: 'high-value-dealer/index',
+    message: 'address where you carry on business activities covered by the Money Laundering Regulations, details of staff and agents.',
+    time: '30',
+    status: undefined
+  },
+  {
+    id: 9,
+    name: 'Money service business',
+    section: 'money-service-business',
+    link: 'money-service-business/index',
+    message: 'details of staff and agents.',
+    time: '30',
+    status: undefined
+  },
+  {
     id: 10,
+    name: 'Trust and company service provider',
+    section: 'trust-company-service-provider',
+    link: 'trust-company-service-provider/index',
+    message: 'activities covered by the Money Laundering Regulations, details of staff and agents.',
+    time: '30',
+    status: undefined
+  },
+    {
+    id: 11,
     name: 'About you',
     section: 'about-you',
     link: 'about-you/index',
@@ -99,7 +108,7 @@ module.exports = [
     status: undefined
   },
   {
-    id: 11,
+    id: 12,
     name: 'Payment',
     section: 'payment',
     link: 'payment/index',
@@ -107,4 +116,5 @@ module.exports = [
     time: '30',
     status: undefined
   }
+
 ];
