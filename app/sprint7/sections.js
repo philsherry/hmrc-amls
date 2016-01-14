5ce2692422ff38db0bef6abd6baa3a7b62b53634
Brian Coult <Etheya@Brians-MacBook-Pro.local>
2016-01-14 11:48:28 +0000

    Added edit return to summary to summary page

--- app/sprint7/sections.js
+++ app/sprint7/sections.js
@@ -4,6 +4,7 @@ module.exports = [
     name: 'Business matching',
     section: 'business-matching',
     link: 'business-matching/index',
+    editlink: 'business-matching/summary',
     message: 'covered by the Money Laundering Regulations.',
     time: '30',
     status: undefined
@@ -13,6 +14,7 @@ module.exports = [
     name: 'About the business',
     section: 'business-details',
     link: 'business-details/index',
+    editlink: 'business-details/summary',
     message: 'business activities covered by the Money Laundering Regulations, details of staff and agents.',
     time: '5',
     status: undefined
@@ -22,6 +24,7 @@ module.exports = [
     name: 'Business activities',
     section: 'business-activities',
     link: 'business-activities/index',
+    editlink: 'business-activities/summary',
     message: 'your details, details of each address where you carry on business activities',
     time: '30',
     status: undefined
@@ -31,6 +34,7 @@ module.exports = [
     name: 'Bank account details',
     section: 'bank-details',
     link: 'bank-details/index',
+    editlink: 'bank-details/summary',
     message: 'passport number, bank account details',
     time: '30',
     status: undefined
@@ -40,6 +44,7 @@ module.exports = [
     name: 'Responsible people',
     section: 'responsible-people',
     link: 'responsible-people/index',
+    editlink: 'responsible-people/summary',
     message: 'passport number, national insurance number.',
     time: '30',
     status: undefined
@@ -49,6 +54,7 @@ module.exports = [
     name: 'Trading premises',
     section: 'trading-premises',
     link: 'trading-premises/index',
+    editlink: 'trading-premises/summary',
     message: 'details of each address where you carry on.',
     time: '30',
     status: undefined
@@ -58,6 +64,7 @@ module.exports = [
     name: 'Accountancy service provider',
     section: 'accountancy-service-provider',
     link: 'accountancy-service-provider/index',
+    editlink: 'accountancy-service-provider/summary',
     message: 'your details, details of each address',
     time: '30',
     status: undefined
@@ -67,6 +74,7 @@ module.exports = [
     name: 'Estate agent business',
     section: 'estate-agents',
     link: 'estate-agents/index',
+    editlink: 'estate-agents/summary',
     message: 'by the Money Laundering Regulations, details of staff and agents.',
     time: '30',
     status: undefined
@@ -76,6 +84,7 @@ module.exports = [
     name: 'High value dealer',
     section: 'high-value-dealer',
     link: 'high-value-dealer/index',
+    editlink: 'high-value-dealer/summary',
     message: 'address where you carry on business activities covered by the Money Laundering Regulations, details of staff and agents.',
     time: '30',
     status: undefined
@@ -85,6 +94,7 @@ module.exports = [
     name: 'Money service business',
     section: 'money-service-business',
     link: 'money-service-business/index',
+    editlink: 'money-service-business/summary',
     message: 'details of staff and agents.',
     time: '30',
     status: undefined
@@ -94,6 +104,7 @@ module.exports = [
     name: 'Trust and company service provider',
     section: 'trust-company-service-provider',
     link: 'trust-company-service-provider/index',
+    editlink: 'trust-company-service-provider/summary',
     message: 'activities covered by the Money Laundering Regulations, details of staff and agents.',
     time: '30',
     status: undefined
@@ -103,6 +114,7 @@ module.exports = [
     name: 'About you',
     section: 'about-you',
     link: 'about-you/index',
+    editlink: 'about-you/summary',
     message: 'your details, details of each address where you carry on business activities covered by the Money Laundering Regulations, details of staff and agents.',
     time: '30',
     status: undefined
@@ -112,6 +124,7 @@ module.exports = [
     name: 'Payment',
     section: 'payment',
     link: 'payment/index',
+    editlink: 'payment/summary',
     message: 'covered by the Money Laundering Regulations.',
     time: '30',
     status: undefined
