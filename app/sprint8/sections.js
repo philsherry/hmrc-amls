0eed1a2c04f4fb8fb6efc10faac70df98bd36cc9
Brian Coult <Etheya@Brians-MacBook-Pro.local>
2016-01-26 13:23:22 +0000

    updated sections.js
    Signed-off-by: Brian Coult <Etheya@Brians-MacBook-Pro.local>

--- app/sprint8/sections.js
+++ app/sprint8/sections.js
@@ -1,6 +1,7 @@
 module.exports = [
   [
     {
+      id: 1,
       name: 'Business matching',
       section: 'business-matching',
       link: 'business-matching/index',
@@ -10,6 +11,7 @@ module.exports = [
       status: undefined
     },
     {
+      id: 2,
       name: 'About the business',
       section: 'business-details',
       link: 'business-details/index',
@@ -19,6 +21,7 @@ module.exports = [
       status: undefined
     },
     {
+      id: 3,
       name: 'Business activities',
       section: 'business-activities',
       link: 'business-activities/index',
@@ -27,7 +30,8 @@ module.exports = [
       time: '23',
       status: undefined
     },
-    { 
+    {
+      id: 4,
       name: 'Bank account details',
       section: 'bank-details',
       link: 'bank-details/index',
@@ -37,6 +41,7 @@ module.exports = [
       status: undefined
     },
     {
+      id: 5,
       name: 'Responsible people',
       section: 'responsible-people',
       link: 'responsible-people/index',
@@ -46,6 +51,7 @@ module.exports = [
       status: undefined
     },
     {
+      id: 6,
       name: 'Trading premises',
       section: 'trading-premises',
       link: 'trading-premises/index',
@@ -57,6 +63,7 @@ module.exports = [
   ],
   [
     {
+      id: 7,
       name: 'Accountancy service provider',
       section: 'accountancy-service-provider',
       link: 'accountancy-service-provider/index',
@@ -66,6 +73,7 @@ module.exports = [
       status: undefined
     },
     {
+      id: 8,
       name: 'Estate agent business',
       section: 'estate-agents',
       link: 'estate-agents/index',
@@ -75,6 +83,7 @@ module.exports = [
       status: undefined
     },
     {
+      id: 9,
       name: 'High value dealer',
       section: 'high-value-dealer',
       link: 'high-value-dealer/index',
@@ -84,6 +93,7 @@ module.exports = [
       status: undefined
     },
     {
+      id: 10,
       name: 'Money service business',
       section: 'money-service-business',
       link: 'money-service-business/index',
@@ -93,6 +103,7 @@ module.exports = [
       status: undefined
     },
     {
+      id: 11,
       name: 'Trust and company service provider',
       section: 'trust-company-service-provider',
       link: 'trust-company-service-provider/index',
@@ -104,6 +115,7 @@ module.exports = [
   ],
   [
     {
+      id: 12,
       name: 'About you',
       section: 'about-you',
       link: 'about-you/index',
@@ -113,6 +125,7 @@ module.exports = [
       status: undefined
     },
     {
+      id: 13,
       name: 'Payment',
       section: 'payment',
       link: 'payment/index',
