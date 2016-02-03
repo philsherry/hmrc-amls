14354ff0fb63ec691efe5b9d2ad95f061374e92d
Brian Coult <Etheya@Brians-MacBook-Pro.local>
2016-02-03 11:07:56 +0000

    Added new sprint and updated userresearch version
    Signed-off-by: Brian Coult <Etheya@Brians-MacBook-Pro.local>

---
+++ app/sprint9/app.js
@@ -0,0 +1,55 @@
+var app = require('../../lib/subapp.js')(__dirname),
+  _ = require('lodash');
+
+app.post('/business-matching/activities', function (req, res) {
+  var sections = require('./sections.js');
+  req.session.sections = [].concat(
+      sections[0],
+      [].concat(req.body.activities).reduce(function (m, n) {
+        var e = sections[1][n];
+        if (e) {
+          return m.concat(e);
+        } else {
+          return m;
+        }
+      }, []),
+      sections[2]
+    );
+  res.redirect(req.body['next-page']);
+});
+
+app.post('/:section/summary', function (req, res) {
+  var sections = req.session.sections.map(
+    function (e) {
+      if (e.section === req.params.section) {
+        e.status = 'DONE';
+      }
+      return e;
+  });
+  req.session.sections = sections;
+  res.redirect(
+    '../summary'
+  );
+});
+
+app.post('/:section/:page', function (req, res) {
+  if (req.session.sections) {
+    var sections = req.session.sections.map(
+      function (e) {
+        if (e.section === req.params.section) {
+          e.status = 'IN PROGRESS';
+        }
+        return e;
+      });
+    req.session.sections = sections;
+  }
+  res.redirect(req.body['next-page']);
+});
+
+app.get('/summary', function (req, res) {
+  res.render('summary', {
+    sections : req.session.sections
+  });
+});
+
+module.exports = app;
